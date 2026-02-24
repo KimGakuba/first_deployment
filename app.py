1c5688bfec9396f879583271626d2ea415b1a80f
NGABONZIZA Kim Gakuba <kimgakuban@gmail.com>
2026-02-24 13:11:04 +0300

    updating app.py

--- app.py
+++ app.py
@@ -581,5 +581,3 @@ if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
     debug = os.getenv('FLASK_ENV') == 'development'
     app.run(host='0.0.0.0', port=port, debug=debug)
- 
- 
